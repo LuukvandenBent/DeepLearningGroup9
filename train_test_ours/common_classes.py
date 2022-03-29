71ff152e63856560a77120782f8b66bcf333283c
Luuk <luukbent@gmail.com>
2022-03-29 18:06:08 +0200

    Added some commented lines that were uncommented in the original SID training dir

--- train_test_ours/common_classes.py
+++ train_test_ours/common_classes.py
@@ -85,6 +85,11 @@ def part_init(gt_files,train_files,num_print,filename,gt_amp=False):
         na5 = get_na(bins,weights5,img_loww)
         
         # load GT amplifications if required and save in ta
+
+	#if gt_files[i][-7]=='3':
+        #    ta=300
+        #else:
+        #    ta=100
         
         H,W = img_loww.shape    
         a = np.float32(np.float64(H*W*0.01)/np.sum(img_loww,dtype=np.float64))
